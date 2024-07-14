9faf3828230af06785aa7b00ac2e92d7254a1877
yseo14 <lmslys14@naver.com>
2024-07-14 17:50:31 +0900

    Chore: Add project

---
+++ src/main/java/com/Cruise/ParkingLotto/ParkingLottoApplication.java
@@ -0,0 +1,13 @@
+package com.Cruise.ParkingLotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLottoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Cruise/ParkingLotto/ParkingLottoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Cruise.ParkingLotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLottoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
