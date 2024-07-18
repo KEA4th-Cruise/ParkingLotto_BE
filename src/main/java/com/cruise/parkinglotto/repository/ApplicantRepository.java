2dbed91599fc836355696dce47cc7af6fc01ba7f
username <junbumchoi0402@gmail.com>
2024-07-18 09:46:22 +0900

    FEAT: 당첨 포기 API 구현

--- src/main/java/com/cruise/parkinglotto/repository/ApplicantRepository.java
+++ src/main/java/com/cruise/parkinglotto/repository/ApplicantRepository.java
@@ -2,9 +2,17 @@ package com.cruise.parkinglotto.repository;
 
 import com.cruise.parkinglotto.domain.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
     List<Applicant> findByDrawId(Long drawId);
+
+    @Query("select a.id from Applicant a where a.member.id = :memberId")
+    Long findByMember(@Param("memberId") Long memberId);
+
+    @Query("select a.parkingSpaceId from Applicant a where a.id =:applicantId")
+    Long findParkingSpaceId(@Param("applicantId") Long applicantId);
 }
