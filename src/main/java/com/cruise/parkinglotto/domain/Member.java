f31a36a5dbc7f1770e7777a1859a7dc6b4d1c560
Haecheol Shin <dev.hc.shin@gmail.com>
2024-07-19 14:12:40 +0900

    Feat: jwt 기능 추가
    - 토큰을 저장하는 JwtToken 클래스 생성
    - 토큰을 생성, 복호화 등을 하는 JwtUtils 클래스 생성
    - Jwt가 유효한지 검사하는 JwtAuthenticationFilter 클래스 생성
    - Jwt 필터를 사용하기 위한 SpringConfig 클래스 생성

--- src/main/java/com/cruise/parkinglotto/domain/Member.java
+++ src/main/java/com/cruise/parkinglotto/domain/Member.java
@@ -6,15 +6,22 @@ import com.cruise.parkinglotto.domain.enums.EnrollmentStatus;
 import com.cruise.parkinglotto.domain.enums.WorkType;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member extends BaseEntity {
+public class Member extends BaseEntity implements UserDetails {
 
     @Id
     @Column(name = "member_id")
@@ -35,10 +42,6 @@ public class Member extends BaseEntity {
 
     @Column(nullable = false)
     private String email;
-
-    @Column(nullable = false)
-    @Enumerated(value = EnumType.STRING)
-    private AccountType accountType;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
@@ -67,4 +70,43 @@ public class Member extends BaseEntity {
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CertificateDocs> certificateDocsList;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private AccountType accountType;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(accountType.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
