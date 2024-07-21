cfc0b14126dc478061eb7ccf4a72539d9068818e
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2024-07-21 13:09:29 +0900

    Style: } 누락으로 인한 변경사항 수정

--- src/main/java/com/cruise/parkinglotto/web/controller/DrawRestController.java
+++ src/main/java/com/cruise/parkinglotto/web/controller/DrawRestController.java
@@ -25,11 +25,12 @@ public class DrawRestController {
     public ApiResponse<Void> executeDraw(@PathVariable Long drawId) {
         drawService.executeDraw(drawId);
         return ApiResponse.onSuccess(SuccessStatus.DRAW_EXECUTE_RESULT, null);
+    }
 
     @Operation(summary = "해당 회차 현재 신청 현황 조회 API", description = "/current-info/{drawId}중 {drawId}에 유효한 Id를 넣으면 해당 추첨에 관한 정보를 반환합니다.")
     @GetMapping("/current-info/{drawId}")
     public ApiResponse<DrawResponseDTO.GetCurrentDrawInfoDTO> getCurrentDrawInfo(HttpServletRequest httpServletRequest,
-                                                                         @PathVariable Long drawId) {
+                                                                                 @PathVariable Long drawId) {
 
         DrawResponseDTO.GetCurrentDrawInfoDTO getCurrentDrawInfoDto = drawService.getCurrentDrawInfo(httpServletRequest, drawId);
 
