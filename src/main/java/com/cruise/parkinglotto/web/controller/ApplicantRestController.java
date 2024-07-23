055c67d87404139101844f4e7cc9dd20ccebc4a2
yseo14 <lmslys14@naver.com>
2024-07-24 02:10:57 +0900

    Chore: 신청자 목록 조회 API URI 변경

--- src/main/java/com/cruise/parkinglotto/web/controller/ApplicantRestController.java
+++ src/main/java/com/cruise/parkinglotto/web/controller/ApplicantRestController.java
@@ -20,11 +20,12 @@ import org.springframework.web.bind.annotation.*;
 public class ApplicantRestController {
     private final ApplicantService applicantService;
 
-    @Operation(summary = "신청자 목록을 조회하는 API 입니다. 페이징을 포함합니다.", description = "PathVariable로 drawId와 RequestParam 으로 page 번호를 전송해주세요.")
-    @GetMapping("/list/{drawId}")
-    public ApiResponse<ApplicantResponseDTO.GetApplicantListResultDTO> getApplicantList(@PathVariable Long drawId,
+    @Operation(summary = "신청자 목록을 조회하는 API 입니다. 페이징을 포함합니다.", description = " RequestParam 으로 drawId와 page 번호를 전송해주세요.")
+    @GetMapping("/list/")
+    public ApiResponse<ApplicantResponseDTO.GetApplicantListResultDTO> getApplicantList(@RequestParam(name = "drawId") Long drawId,
                                                                                         @RequestParam(name = "page") Integer page) {
         Page<Applicant> applicantList = applicantService.getApplicantList(page - 1, drawId);
         return ApiResponse.onSuccess(SuccessStatus.APPLICANT_LIST_FOUND, ApplicantConverter.toGetApplicantListResultDTO(applicantList));
     }
+
 }
