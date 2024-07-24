d2ff05907ed6b217f108d12a83313112fb5e05d2
Junbum Choi <117131532+Junbum-hub@users.noreply.github.com>
2024-07-24 14:50:17 +0900

    Fix: jwtUtil

--- src/main/java/com/cruise/parkinglotto/web/dto/drawDTO/DrawResponseDTO.java
+++ src/main/java/com/cruise/parkinglotto/web/dto/drawDTO/DrawResponseDTO.java
@@ -1,66 +1,32 @@
 package com.cruise.parkinglotto.web.dto.drawDTO;
 
-import com.cruise.parkinglotto.domain.enums.DrawType;
-import com.cruise.parkinglotto.web.dto.applicantDTO.ApplicantResponseDTO;
-import com.cruise.parkinglotto.web.dto.parkingSpaceDTO.ParkingSpaceResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import com.cruise.parkinglotto.web.dto.parkingSpaceDTO.ParkingSpaceResponseDTO.GetNameAndUrlParkingSpaceResultDTO;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
-public class DrawResponseDTO {
+public class DrawResponseDto {
 
     @Getter
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class GetCurrentDrawInfoDTO {
-        private LocalDateTime drawStartAt;
-        private LocalDateTime drawEndAt;
-        private LocalDateTime usageStartAt;
-        private LocalDateTime usageEndAt;
-        private String mapImageUrl;
-        private List<GetNameAndUrlParkingSpaceResultDTO> getNameAndUrlParkingSpaceResultDTO;
+    public static class GetWinnerMemberDto {
+        private Long drawId;
+        private String name_ko;
+        private String account_id;
+        private String depth_path_name;
+        private Long reserve_num;
+        private Double weighted_total_score;
     }
 
     @Getter
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class DrawResultResponseDTO {
-        private Long drawId;
-        private String drawType;
-        private String title;
-        private String seedNum;
-        private Integer totalSlots;
-        private String year;
-        private String quarter;
-        private List<ApplicantResponseDTO.ApplicantResultDTO> applicants;
-    }
-
-    @Getter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class CreateDrawResultDTO {
-        private Long drawId;
-        private String title;
-        private LocalDateTime createdAt;
-    }
-
-    @Getter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ConfirmDrawCreationResultDTO{
-        private Long drawId;
-        private String title;
-        LocalDateTime usageStartAt;
-        LocalDateTime usageEndAt;
-        ParkingSpaceResponseDTO.ParkingSpacePreviewListDTO parkingSpacePreviewListDTO;
+    public static class GetDrawResultPersonListDto{
+        private List<GetWinnerMemberDto> drawResultDtoList;
     }
 }
