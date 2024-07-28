1b132ff0477cc8810432a17ed43e24a0dc176e01
Haecheol Shin <dev.hc.shin@gmail.com>
2024-07-28 15:26:34 +0900

    Feat: 사용자 등록 요청 승인 API 구현
    - 관리자가 사용자 세부 정보 페이지에서 등록 요청을 승인하는 API를 구현했습니다.

--- src/main/java/com/cruise/parkinglotto/service/registerService/RegisterServiceImpl.java
+++ src/main/java/com/cruise/parkinglotto/service/registerService/RegisterServiceImpl.java
@@ -32,6 +32,16 @@ public class RegisterServiceImpl implements RegisterService {
     }
 
     @Override
+    @Transactional
+    public Object approveRegister(Member member) {
+        int updatedCount = memberRepository.updateEnrollmentStatusToEnrolled(member.getAccountId());
+        if (updatedCount == 0) { // 관리자가 승인을 했는데 ENROLLED로 바뀌지 않은 경우
+            throw new ExceptionHandler(ErrorStatus.REGISTER_REQUEST_FAILED);
+        }
+        return null;
+    }
+
+    @Override
     @Transactional(readOnly = true)
     public RegisterResponseDTO.MemberInfoResponseDTO getMemberInfo(String accountId) {
         Member member = memberService.getMemberByAccountId(accountId);
