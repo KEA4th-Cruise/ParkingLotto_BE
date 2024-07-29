4d3812dc013a1d729557e74858fe337eed68ffc8
yseo14 <lmslys14@naver.com>
2024-07-29 15:01:55 +0900

    Refactor: ErrorStatus HttpStatus 수정

--- src/main/java/com/cruise/parkinglotto/global/response/code/status/ErrorStatus.java
+++ src/main/java/com/cruise/parkinglotto/global/response/code/status/ErrorStatus.java
@@ -19,26 +19,26 @@ public enum ErrorStatus implements BaseErrorCode {
     _EMPTY_FIELD(HttpStatus.NO_CONTENT, "COMMON404", "입력 값이 누락되었습니다."),
 
     // 멤버 관련 응답
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER4001", "사용자가 존재하지 않습니다"),
-    MEMBER_PASSWORD_NOT_MATCHED(HttpStatus.BAD_REQUEST, "MEMBER4002", "비밀번호가 일치하지 않습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.OK, "MEMBER4001", "사용자가 존재하지 않습니다"),
+    MEMBER_PASSWORD_NOT_MATCHED(HttpStatus.OK, "MEMBER4002", "비밀번호가 일치하지 않습니다."),
 
     // 등록 관련 응답
-    REGISTER_REQUEST_FAILED(HttpStatus.BAD_REQUEST, "REGISTER4001", "등록 요청에 실패했습니다."),
-    REGISTER_MEMBERS_NOT_FOUND(HttpStatus.BAD_REQUEST, "REGISTER4002", "enrollmentStatus가 잘못되었습니다."),
-    REGISTER_APPROVE_FAILED(HttpStatus.BAD_REQUEST, "REGISTER4003", "등록 요청 승인에 실패했습니다."),
-    REGISTER_REFUSE_FAILED(HttpStatus.BAD_REQUEST, "REGISTER4004", "등록 요청 거절에 실패했습니다."),
+    REGISTER_REQUEST_FAILED(HttpStatus.OK, "REGISTER4001", "등록 요청에 실패했습니다."),
+    REGISTER_MEMBERS_NOT_FOUND(HttpStatus.OK, "REGISTER4002", "enrollmentStatus가 잘못되었습니다."),
+    REGISTER_APPROVE_FAILED(HttpStatus.OK, "REGISTER4003", "등록 요청 승인에 실패했습니다."),
+    REGISTER_REFUSE_FAILED(HttpStatus.OK, "REGISTER4004", "등록 요청 거절에 실패했습니다."),
 
     //신청자 관련 응답
-    APPLICANT_NOT_FOUND(HttpStatus.NOT_FOUND, "APPLICANT4001", "신청자가 존재하지 않습니다"),
+    APPLICANT_NOT_FOUND(HttpStatus.OK, "APPLICANT4001", "신청자가 존재하지 않습니다"),
 
     // 주차 공간 관련 응답
-    PARKING_SPACE_NOT_FOUND(HttpStatus.NOT_FOUND, "PARKINGSPACE4001", "주차 공간이 존재하지 않습니다"),
-    NO_REMAIN_SLOTS(HttpStatus.BAD_REQUEST, "PARKINGSPACE4002", "남은 주차공간이 없습니다."),
+    PARKING_SPACE_NOT_FOUND(HttpStatus.OK, "PARKINGSPACE4001", "주차 공간이 존재하지 않습니다"),
+    NO_REMAIN_SLOTS(HttpStatus.OK, "PARKINGSPACE4002", "남은 주차공간이 없습니다."),
 
     // 추첨 관련 응답
-    DRAW_NOT_FOUND(HttpStatus.NOT_FOUND, "DRAW4001", "추첨이 존재하지 않습니다."),
-    DRAW_NOT_READY(HttpStatus.NOT_FOUND, "DRAW4002", "아직 신청이 종료되지 않은 추첨입니다."),
-    DRAW_ALREADY_EXECUTED(HttpStatus.NOT_FOUND, "DRAW4003", "이미 종료된 추첨입니다. 한 번 진행된 추첨은 다시 진행 될 수 없습니다."),
+    DRAW_NOT_FOUND(HttpStatus.OK, "DRAW4001", "추첨이 존재하지 않습니다."),
+    DRAW_NOT_READY(HttpStatus.OK, "DRAW4002", "아직 신청이 종료되지 않은 추첨입니다."),
+    DRAW_ALREADY_EXECUTED(HttpStatus.OK, "DRAW4003", "이미 종료된 추첨입니다. 한 번 진행된 추첨은 다시 진행 될 수 없습니다."),
 
 
     ;
