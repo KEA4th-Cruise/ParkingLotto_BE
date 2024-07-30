369d65d16f29d919bc9b89153244cfb607f07519
yseo14 <lmslys14@naver.com>
2024-07-30 14:21:05 +0900

    Refactor: Swagger에서 RequestPart 이미지 첨부 가능하도록 수정

--- src/main/java/com/cruise/parkinglotto/web/controller/ParkingSpaceRestController.java
+++ src/main/java/com/cruise/parkinglotto/web/controller/ParkingSpaceRestController.java
@@ -8,6 +8,7 @@ import com.cruise.parkinglotto.web.converter.ParkingSpaceConverter;
 import com.cruise.parkinglotto.web.dto.parkingSpaceDTO.ParkingSpaceRequestDTO;
 import com.cruise.parkinglotto.web.dto.parkingSpaceDTO.ParkingSpaceResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,10 +18,10 @@ import org.springframework.web.multipart.MultipartFile;
 public class ParkingSpaceRestController {
     private final ParkingSpaceService parkingSpaceService;
 
-    @PostMapping("/{drawId}")
+    @PostMapping(value = "/{drawId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponse<ParkingSpaceResponseDTO.AddParkingSpaceResultDTO> addParkingSpace(@PathVariable Long drawId,
                                                                                          @RequestPart(value = "floorPlanImage", required = true) MultipartFile floorPlanImage,
-                                                                                         @RequestPart(value = "addParkingSpaceDTO", required = true)ParkingSpaceRequestDTO.AddParkingSpaceDTO addParkingSpaceDTO) {
+                                                                                         @RequestPart(value = "addParkingSpaceDTO", required = true) ParkingSpaceRequestDTO.AddParkingSpaceDTO addParkingSpaceDTO) {
         ParkingSpace parkingSpace = parkingSpaceService.addParkingSpace(drawId, floorPlanImage, addParkingSpaceDTO);
         return ApiResponse.onSuccess(SuccessStatus.PARKING_SPACE_ADDED, ParkingSpaceConverter.toAddParkingSpaceResultDTO(parkingSpace));
     }
@@ -28,7 +29,7 @@ public class ParkingSpaceRestController {
     @GetMapping("/myspace/{memberId}")
     public ApiResponse<ParkingSpaceResponseDTO.ParkingSpaceInfoResponseDTO> getParkingSpaceInfo(@PathVariable("memberId") Long memberId) {
 
-        return ApiResponse.onSuccess(SuccessStatus.PARKING_SPACE_INFO_FOUND,parkingSpaceService.findParkingSpaceInfo(memberId));
+        return ApiResponse.onSuccess(SuccessStatus.PARKING_SPACE_INFO_FOUND, parkingSpaceService.findParkingSpaceInfo(memberId));
     }
 
 }
