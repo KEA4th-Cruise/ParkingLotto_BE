20736154730dcb6b49297fa4c89c70379cd1685f
LeeJungKyun <76837194+LeeJungKyun@users.noreply.github.com>
2024-07-31 08:47:10 +0900

    Chore: Operation으로 컨트롤러에 설명 추가
    Summary랑 Description 작성 안되어있던 부분 추가

--- src/main/java/com/cruise/parkinglotto/web/controller/DrawRestController.java
+++ src/main/java/com/cruise/parkinglotto/web/controller/DrawRestController.java
@@ -23,15 +23,17 @@ public class DrawRestController {
     private final HttpServletRequest httpServletRequest;
 
     //추첨 실행 후 결과 저장하는 API
+    @Operation(summary = "추첨 실행 API", description = "path variable로 추첨 ID를 넘겨주면 해당 추첨을 실행합니다.")
     @PostMapping("/execution/{drawId}")
-    public ApiResponse<Void> executeDraw(@PathVariable Long drawId) {
+    public ApiResponse<Void> executeDraw(@PathVariable("drawId") Long drawId) {
         drawService.executeDraw(drawId);
         return ApiResponse.onSuccess(SuccessStatus.DRAW_EXECUTE_RESULT, null);
     }
 
     //회차의 추첨 결과의 명단을 조회하는 API
+    @Operation(summary = "해당 회차 추첨 결과 조회 API", description = "path variable로 추첨 ID를 넘겨주면 해당 추첨의 결과를 반환합니다.")
     @GetMapping("/result-members/{drawId}")
-    public ApiResponse<DrawResponseDTO.DrawResultResponseDTO> getDrawResult(HttpServletRequest httpServletRequest, @PathVariable Long drawId) {
+    public ApiResponse<DrawResponseDTO.DrawResultResponseDTO> getDrawResult(HttpServletRequest httpServletRequest, @PathVariable("drawId") Long drawId) {
 
         DrawResponseDTO.DrawResultResponseDTO drawResultResponseDTO = drawService.getDrawResult(httpServletRequest, drawId);
         return ApiResponse.onSuccess(SuccessStatus.DRAW_INFO_FOUND, drawResultResponseDTO);
@@ -60,5 +62,12 @@ public class DrawRestController {
     public ApiResponse<DrawResponseDTO.ConfirmDrawCreationResultDTO> confirmDrawCreation(@PathVariable Long drawId) {
         DrawResponseDTO.ConfirmDrawCreationResultDTO drawCreationResultDTO = drawService.confirmDrawCreation(drawId);
         return ApiResponse.onSuccess(SuccessStatus.DRAW_CREATION_CONFIRMED, drawCreationResultDTO);
+    }
+
+    @Operation(summary = "페이징을 포함한 추첨 시뮬레이션 API", description = "path variable로 drawId, RequestParam으로 seedNum과 페이지를 넘겨주면 추첨 시뮬레이션을 실행합니다.")
+    @GetMapping("/{drawId}/simulate")
+    public ApiResponse<DrawResponseDTO.SimulateDrawResponseDTO> simulateDraw(@PathVariable("drawId") Long drawId, @RequestParam("seedNum") String seedNum, @RequestParam("page") Integer page) {
+        DrawResponseDTO.SimulateDrawResponseDTO simulateDrawResponseDTO = drawService.simulateDraw(drawId, seedNum, page);
+        return ApiResponse.onSuccess(SuccessStatus.DRAW_SIMULATE_COMPLETED, simulateDrawResponseDTO);
     }
 }
