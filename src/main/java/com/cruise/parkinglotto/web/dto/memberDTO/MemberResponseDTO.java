06e734ca1832d33a0d28fc664073566c565c9d31
Junbum Choi <117131532+Junbum-hub@users.noreply.github.com>
2024-07-31 11:04:33 +0900

    Feat: MemberResponseDTO 에 MyInfoResponseDTO 와 MyCertificationInfoResponseDTO 추가

--- src/main/java/com/cruise/parkinglotto/web/dto/memberDTO/MemberResponseDTO.java
+++ src/main/java/com/cruise/parkinglotto/web/dto/memberDTO/MemberResponseDTO.java
@@ -1,6 +1,7 @@
 package com.cruise.parkinglotto.web.dto.memberDTO;
 
 import com.cruise.parkinglotto.domain.enums.EnrollmentStatus;
+import com.cruise.parkinglotto.domain.enums.WorkType;
 import com.cruise.parkinglotto.global.jwt.JwtToken;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MemberResponseDTO {
 
@@ -28,4 +30,28 @@ public class MemberResponseDTO {
     public static class LogoutResponseDTO {
         private LocalDateTime logoutAt;
     }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyInfoResponseDTO {
+        private String carNum;
+        private List<MyCertificationInfoResponseDTO> myCertificationInfoResponseDTOS;
+        private String address;
+        private WorkType workType;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyCertificationInfoResponseDTO {
+
+        private Long certificateDocsId;
+        private String fileUrl;
+        private String fileName;
+
+    }
+
 }
