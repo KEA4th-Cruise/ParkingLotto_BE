16d25d401322cd6009110623232a4cebbb6c575f
Junbum Choi <117131532+Junbum-hub@users.noreply.github.com>
2024-07-31 11:08:05 +0900

    Feat: 내가 입력한 정보를 불러오는 API 를 개발

--- src/main/java/com/cruise/parkinglotto/web/controller/MemberRestController.java
+++ src/main/java/com/cruise/parkinglotto/web/controller/MemberRestController.java
@@ -1,11 +1,14 @@
 package com.cruise.parkinglotto.web.controller;
 
+import com.cruise.parkinglotto.domain.Member;
+import com.cruise.parkinglotto.global.jwt.JwtUtils;
 import com.cruise.parkinglotto.global.response.ApiResponse;
 import com.cruise.parkinglotto.global.response.code.status.SuccessStatus;
 import com.cruise.parkinglotto.service.memberService.MemberService;
 import com.cruise.parkinglotto.web.dto.memberDTO.MemberRequestDTO;
 import com.cruise.parkinglotto.web.dto.memberDTO.MemberResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class MemberRestController {
 
     private final MemberService memberService;
+    private final JwtUtils jwtUtils;
 
     @Operation(summary = "로그인 API", description = "accountId, password를 loginRequestDTO에 담아 요청을 보내면 등록 여부와 토큰을 반환합니다.")
     @PostMapping("/login")
@@ -27,4 +31,15 @@ public class MemberRestController {
     public ApiResponse<MemberResponseDTO.LogoutResponseDTO> logout(@RequestBody MemberRequestDTO.LogoutRequestDTO logoutRequestDTO) {
         return ApiResponse.onSuccess(SuccessStatus.MEMBER_LOGOUT_SUCCESS, memberService.logout(logoutRequestDTO));
     }
+
+    @Operation(summary = "내가 입력한 정보 API",description = "내가 이전에 입력했던 회원정보를 불러오는 API 입니다. 주요 정보로는 차량번호, 첨부 파일, 거주지주소, 근무타입이 있습니다.")
+    @GetMapping("/my-info")
+    public ApiResponse<MemberResponseDTO.MyInfoResponseDTO> getMyInfo(HttpServletRequest httpServletRequest) {
+
+        String accountIdFromRequest = jwtUtils.getAccountIdFromRequest(httpServletRequest);
+        Member memberByAccountId = memberService.getMemberByAccountId(accountIdFromRequest);
+        return ApiResponse.onSuccess(SuccessStatus.MEMBER_INFO_FOUND, memberService.getMyInfo(memberByAccountId.getId()));
+
+    }
+
 }
