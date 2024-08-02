fc582b3114416a99c938d819ae0f12ad2baf6d87
Haecheol Shin <dev.hc.shin@gmail.com>
2024-08-02 18:03:04 +0900

    Feat: 로그인 필드값 검증 기능 추가
    - 로그인에 필요한 아이디(사원명)와 비밀번호에 필드 값 검증 기능이 추가되었습니다.

--- src/main/java/com/cruise/parkinglotto/web/controller/MemberRestController.java
+++ src/main/java/com/cruise/parkinglotto/web/controller/MemberRestController.java
@@ -1,6 +1,7 @@
 package com.cruise.parkinglotto.web.controller;
 
 import com.cruise.parkinglotto.domain.Member;
+import com.cruise.parkinglotto.global.jwt.JwtToken;
 import com.cruise.parkinglotto.global.jwt.JwtUtils;
 import com.cruise.parkinglotto.global.response.ApiResponse;
 import com.cruise.parkinglotto.global.response.code.status.SuccessStatus;
@@ -12,7 +13,10 @@ import com.cruise.parkinglotto.web.dto.memberDTO.MemberRequestDTO;
 import com.cruise.parkinglotto.web.dto.memberDTO.MemberResponseDTO;
 import com.cruise.parkinglotto.web.dto.parkingSpaceDTO.ParkingSpaceResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,8 +35,10 @@ public class MemberRestController {
 
     @Operation(summary = "로그인 API", description = "accountId, password를 loginRequestDTO에 담아 요청을 보내면 등록 여부와 토큰을 반환합니다.")
     @PostMapping("/login")
-    public ApiResponse<MemberResponseDTO.LoginResponseDTO> login(@RequestBody MemberRequestDTO.LoginRequestDTO loginRequestDTO) {
-        return ApiResponse.onSuccess(SuccessStatus.MEMBER_LOGIN_SUCCESS, memberService.login(loginRequestDTO));
+    public ApiResponse<MemberResponseDTO.LoginResponseDTO> login(@RequestBody @Valid MemberRequestDTO.LoginRequestDTO loginRequestDTO, HttpServletResponse httpServletResponse) {
+        MemberResponseDTO.LoginResponseDTO loginResponseDTO = memberService.login(loginRequestDTO);
+
+        return ApiResponse.onSuccess(SuccessStatus.MEMBER_LOGIN_SUCCESS, loginResponseDTO);
     }
 
     @Operation(summary = "로그아웃 API", description = "accountId, accessToken, refreshToken을 logoutRequestDTO에 담아 요청을 보내면 로그아웃 시간을 반환합니다.")
@@ -77,4 +83,5 @@ public class MemberRestController {
         return ApiResponse.onSuccess(SuccessStatus.APPLICANT_APPLY_INFO_FOUND, applicantService.getMyApplyInfo(memberByAccountId.getId(), drawId));
 
     }
+
 }
--- src/main/java/com/cruise/parkinglotto/web/dto/memberDTO/MemberRequestDTO.java
+++ src/main/java/com/cruise/parkinglotto/web/dto/memberDTO/MemberRequestDTO.java
@@ -1,5 +1,7 @@
 package com.cruise.parkinglotto.web.dto.memberDTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +14,15 @@ public class MemberRequestDTO {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class LoginRequestDTO {
+
+        @NotBlank(message = "사원명은 필수 입력 값 입니다.")
+        @Pattern(regexp="^(?=.*[a-z])(?=.*\\d)[a-zA-Z\\d.]{3,15}$\n",
+                message = "영문 소문자와 숫자가 적어도 1개 이상씩 포함된 3자 ~ 15자의 사원명을 입력해주세요.")
         private String accountId;
+
+        @NotBlank(message = "비밀번호는 필수 입력 값 입니다.")
+        @Pattern(regexp="^(?=.*[a-z])(?=.*\\d)[a-zA-Z\\d]{9,16}$\n",
+                message = "영문 소문자와 숫자가 적어도 1개 이상씩 포함된 9자 ~ 16자의 비밀번호를 입력해주세요.")
         private String password;
     }
 
