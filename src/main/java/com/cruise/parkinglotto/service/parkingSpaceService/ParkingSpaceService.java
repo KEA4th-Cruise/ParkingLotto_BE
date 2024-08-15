64e5550eddd47bc81bd7d4da4f9adcac954e467f
Junbum Choi <117131532+Junbum-hub@users.noreply.github.com>
2024-08-15 16:11:49 +0900

    Refactor: import 제거

--- src/main/java/com/cruise/parkinglotto/service/parkingSpaceService/ParkingSpaceService.java
+++ src/main/java/com/cruise/parkinglotto/service/parkingSpaceService/ParkingSpaceService.java
@@ -3,7 +3,6 @@ package com.cruise.parkinglotto.service.parkingSpaceService;
 import com.cruise.parkinglotto.domain.ParkingSpace;
 import com.cruise.parkinglotto.web.dto.parkingSpaceDTO.ParkingSpaceRequestDTO;
 import com.cruise.parkinglotto.web.dto.parkingSpaceDTO.ParkingSpaceResponseDTO;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ParkingSpaceService {
